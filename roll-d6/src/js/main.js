276d8b8351f361e0d5080fa356db605405a56da9
Paolaski <paolaski@users.noreply.github.com>
2021-09-30 11:36:07 +0200

    Delete roll-d6 directory

--- roll-d6/src/js/main.js
+++
@@ -1,6 +0,0 @@
-import '../css/style.scss'
-
-const getRandom = () => Math.floor(Math.random() * 6) + 1;
-const getResult = () => document.getElementById("rollresult").innerHTML = getRandom();
-
-document.getElementById("rolldice").addEventListener("click", () => { getResult()});
