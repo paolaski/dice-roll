ab0a3616a9d92af7bc65e9e5787f7f5d90c7023c
Paula P. T <paulapereztafalla@gmail.com>
2021-09-30 10:52:28 +0200

    Dice roll base

---
+++ roll-d6/src/js/main.js
@@ -0,0 +1,6 @@
+import '../css/style.scss'
+
+const getRandom = () => Math.floor(Math.random() * 6) + 1;
+const getResult = () => document.getElementById("rollresult").innerHTML = getRandom();
+
+document.getElementById("rolldice").addEventListener("click", () => { getResult()});
